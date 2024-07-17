9eb7830e43c19110c86daea4af556de1ccf82a92
Saurabh Soni <sonisaurabhsoni333@gmail.com>
2024-07-17 12:03:12 +0530

    initial commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.json('Hello World');
 });
 
+app.get('/server/:id', (req, res) => {
+    res.json(req.params);
+});
+
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
