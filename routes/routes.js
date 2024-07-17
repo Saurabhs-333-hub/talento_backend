7de5679994ec961380eec6042a65773b336862f4
Saurabh Soni <sonisaurabhsoni333@gmail.com>
2024-07-17 16:58:26 +0530

    added get glimpses

--- routes/routes.js
+++ routes/routes.js
@@ -19,4 +19,12 @@ router.get('/posts', async (req, res) => {
     res.json(posts);
 });
 
+router.get('/glimpses', async (req, res) => {
+    const glimpses = await AppwriteConfig.database.listDocuments(
+        AppwriteConstants.databaseId,
+        AppwriteConstants.collectionIdGlimpses,
+    );
+    res.json(glimpses);
+});
+
 export default router;
